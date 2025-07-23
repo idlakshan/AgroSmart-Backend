f29c6b9b2f4f444a9c2f4602804c5d53a217dc6f
idlakshan <id.lakshan21@gmail.com>
2025-07-23 16:00:22 +0530

    refactor: switch weather stats from 6 months to annual data

--- src/application/weather.js
+++ src/application/weather.js
@@ -4,27 +4,36 @@ import { ApiError } from "../domain/errors/ApiError.js";
 
 export const getWeatherStats = async (district) => {
   const location = districts[district];
- if (!districts[district]) {
-  throw new ApiError(400, "Invalid district");
-}
+
+  if (!location) {
+    throw new ApiError(400, "Invalid district");
+  }
 
   const { lat, lon } = location;
 
-  const end = new Date().toISOString().split("T")[0];
-  const start = new Date(new Date().setMonth(new Date().getMonth() - 6)).toISOString().split("T")[0];
 
-  const url = `https://archive-api.open-meteo.com/v1/archive?latitude=${lat}&longitude=${lon}&start_date=${start}&end_date=${end}&daily=temperature_2m_mean,relative_humidity_2m_mean&timezone=auto`;
+  const end = new Date().toISOString().split("T")[0];
+  const start = new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toISOString().split("T")[0];
+
+
+  const url = `https://archive-api.open-meteo.com/v1/archive?latitude=${lat}&longitude=${lon}&start_date=${start}&end_date=${end}&daily=temperature_2m_mean,relative_humidity_2m_mean,precipitation_sum&timezone=auto`;
 
   const res = await axios.get(url);
   const data = res.data.daily;
 
   return {
-    avg_temp: average(data.temperature_2m_mean),
-    avg_humidity: average(data.relative_humidity_2m_mean)
+    avg_temp_annual: average(data.temperature_2m_mean),           
+    avg_humidity_annual: average(data.relative_humidity_2m_mean), 
+    total_rainfall_annual: sum(data.precipitation_sum)            
   };
 };
+
 
 const average = (arr) => {
   const total = arr.reduce((sum, value) => sum + value, 0);
   return +(total / arr.length).toFixed(2);
+};
+
+const sum = (arr) => {
+  return +arr.reduce((sum, value) => sum + value, 0).toFixed(2);
 };
